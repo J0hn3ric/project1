dd6793cfa005a61e4f3c65c9b32a749ce77cb311
J0hn3ric <john.eric.arriola@gmail.com>
2022-09-18 21:35:56 +0200

    fixed hotkeys

--- listener.py
+++ listener.py
@@ -1,19 +1,37 @@
+
 import sys
 from pynput import keyboard
 import time
 
 control = keyboard.Controller()
 
-def combo():
-    time.sleep(1.2)
-    control.tap(keyboard.Key.cmd_l)
-    print("windows button pressed")
+COMBINATION = {keyboard.Key.ctrl, keyboard.Key.alt, keyboard.KeyCode.from_char('l')}
 
-def esc():
-    print("exit")
-    sys.exit(0)
+current = set()
 
-with keyboard.GlobalHotKeys({
-        '<ctrl>+<alt>+h': combo,
-        'x': esc}) as hotkeys:
-    hotkeys.join()
+
+def on_press(key):
+    key=listener.canonical(key)
+    print(key)
+    if key in COMBINATION:
+        current.add(key)
+        if all(k in current for k in COMBINATION):
+            time.sleep(1.2)
+            control.tap(keyboard.Key.cmd_l)
+            current.remove(key)
+            print("windows key pressed")
+    if key == keyboard.KeyCode.from_char('x'):
+        print("exit")
+        listener.stop()
+        sys.exit(0)
+
+
+def on_release(key):
+    try:
+        current.remove(key)
+    except KeyError:
+        pass
+
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
